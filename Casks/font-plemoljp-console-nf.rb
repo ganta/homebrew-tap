e7c46a82152c88f3426f8a78b2455b09e4923280
Hideki Igarashi <hideki.develop@gmail.com>
2022-07-31 03:03:40 +0900

    feat: Add PlemolJP font cask

---
+++ Casks/font-plemoljp-console-nf.rb
@@ -0,0 +1,26 @@
+cask "font-plemoljp-console-nf" do
+  version "1.2.5"
+  sha256 "16edc7c760211e91889bb22349080ea91ed96c7d7aa2920c727e9b2cf81ccd2e"
+
+  url "https://github.com/yuru7/PlemolJP/releases/download/v#{version}/PlemolJP_NF_v#{version}.zip"
+  name "PlemolJP"
+  homepage "https://github.com/yuru7/PlemolJP"
+
+  # PlemolJP Console Nerd Font
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Bold.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-BoldItalic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-ExtraLight.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-ExtraLightItalic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Italic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Light.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-LightItalic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Medium.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-MediumItalic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Regular.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-SemiBold.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-SemiBoldItalic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Text.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-TextItalic.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-Thin.ttf"
+  font "PlemolJP_NF_v#{version}/PlemolJPConsole_NF/PlemolJPConsoleNF-ThinItalic.ttf"
+end
---
+++ Casks/font-plemoljp-hs.rb
@@ -0,0 +1,26 @@
+cask "font-plemoljp-hs" do
+  version "1.2.5"
+  sha256 "31901129157dac2f96f3be415980be5194a1d802e882688bf6aca64b52bd0385"
+
+  url "https://github.com/yuru7/PlemolJP/releases/download/v#{version}/PlemolJP_HS_v#{version}.zip"
+  name "PlemolJP"
+  homepage "https://github.com/yuru7/PlemolJP"
+
+  # PlemolJP Hidden Space
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Bold.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-BoldItalic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-ExtraLight.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-ExtraLightItalic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Italic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Light.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-LightItalic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Medium.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-MediumItalic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Regular.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-SemiBold.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-SemiBoldItalic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Text.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-TextItalic.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-Thin.ttf"
+  font "PlemolJP_HS_v#{version}/PlemolJP_HS/PlemolJPHS-ThinItalic.ttf"
+end
